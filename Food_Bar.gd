72c1c9107b6809f00ee649831a6b0ecef4521082
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2023-10-03 14:50:45 +1300

    Add first session stuff

---
+++ Food_Bar.gd
@@ -0,0 +1,15 @@
+extends TextureProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_food_val_value_changed(value):
+	self.value = value
