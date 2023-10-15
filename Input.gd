cc5aef3bded593340cdc757b50752366bcd63aa8
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2023-10-14 22:46:58 -0700

    More arrays
    Continue playing with arrays. Got it to save. Now make the spinwheels talk to their respective lists.

--- Input.gd
+++ Input.gd
@@ -1,46 +1,17 @@
 extends Node
 
-var save_path = "user://user_data.save"
+var save_file_path = "user://save/"
+var save_file_name = "UserData.tres"
 
-var Food_Hist = []
-var Water_Hist = []
-var Sleep_Hist = []
-var Stress_Hist = []
-var Active_Hist = []
+var userData = UserData.new()
 
-		
 func _ready():
-	# get the list of values objects?
-	pass
+	verify_save_dir(save_file_path)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
+func verify_save_dir(path : String):
+	DirAccess.make_dir_absolute(path)
 
 func _on_input_button_pressed():
-	save()
-
-func save():
-	var file = FileAccess.open(save_path, FileAccess.WRITE)
-	file.store_var(Food_Hist)
-	file.store_var(Water_Hist)
-	file.store_var(Sleep_Hist)
-	file.store_var(Stress_Hist)
-	file.store_var(Active_Hist)
 	
-func load_data():
-	if FileAccess.file_exists(save_path):
-		var file = FileAccess.open(save_path, FileAccess.READ)
-		Food_Hist = file.get_var(Food_Hist)
-		Water_Hist = file.get_var(Water_Hist)
-		Sleep_Hist = file.get_var(Sleep_Hist)
-		Stress_Hist = file.get_var(Stress_Hist)
-		Active_Hist = file.get_var(Active_Hist)
-	else:
-		print("No data saved...")
-		var Food_Hist = []
-		var Water_Hist = []
-		var Sleep_Hist = []
-		var Stress_Hist = []
-		var Active_Hist = []
+	ResourceSaver.save(UserData, save_file_path + save_file_name)
+
---
+++ Resources/UserData.gd
@@ -0,0 +1,23 @@
+extends Resource
+class_name UserData
+
+@export var food_hist = []
+@export var water_hist = []
+@export var sleep_hist = []
+@export var stress_hist = []
+@export var active_hist = []
+
+func update_food(value : int):
+	food_hist.append(value)
+	
+func update_water(value : int):
+	water_hist.append(value)
+	
+func update_sleep(value : int):
+	sleep_hist.append(value)
+	
+func update_stress(value: int):
+	stress_hist.append(value)
+	
+func update_active(value : int):
+	active_hist.append(value)
