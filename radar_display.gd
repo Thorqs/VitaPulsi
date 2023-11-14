732551e39e34eccd0d3aa285564ae11cff9fdbce
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2023-11-14 00:19:57 -0800

    We made buckets!
    Just need to change formatting of end results

--- radar_display.gd
+++ radar_display.gd
@@ -13,6 +13,9 @@ var water_label: Label
 var sleep_label: Label
 var stress_label: Label
 var activity_label: Label
+var WYDW_label: Label
+var WYDOK_label: Label
+var WYDP_label: Label
 
 const radar_offset = Vector2(270, 250)
 const radar_scaling = 20
@@ -46,6 +49,10 @@ func _ready():
 	ring_labels.append(get_node("6"))
 	ring_labels.append(get_node("8"))
 	ring_labels.append(get_node("10"))
+	
+	WYDW_label = get_node("VBoxContainer/Answer_WYDW/WYDW_A_Label")
+	WYDOK_label = get_node("VBoxContainer/Answer_WYDOK/WYDOK_A_Label")
+	WYDP_label = get_node("VBoxContainer/Answer_WYDP/WYDP_A_Label")
 	
 	# make sure they are in the corect position
 	for i in range (1, 11):
@@ -93,37 +100,46 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var food_mean = 0
-	var water_mean = 0
-	var sleep_mean = 0
-	var stress_mean = 0
-	var active_mean = 0
+	var means = [0, 0, 0, 0, 0]
+	var meanbeans = ["Food", "Water", "Sleep", "Stress", "Activity"]
 	if len(data.food_hist) > 0:
 		for entry in data.food_hist:
-			food_mean += entry
-		food_mean = food_mean/len(data.food_hist)
+			means[0] += entry
+		means[0] = means[0]/len(data.food_hist)
 		
 		for entry in data.water_hist:
-			water_mean += entry
-		water_mean = water_mean/len(data.water_hist)
+			means[1] += entry
+		means[1] = means[1]/len(data.water_hist)
 		
 		for entry in data.sleep_hist:
-			sleep_mean += entry
-		sleep_mean = sleep_mean/len(data.sleep_hist)
+			means[2] += entry
+		means[2] = means[2]/len(data.sleep_hist)
 		
 		for entry in data.stress_hist:
-			stress_mean += entry
-		stress_mean = stress_mean/len(data.stress_hist)
+			means[3] += entry
+		means[3] = means[3]/len(data.stress_hist)
 		
 		for entry in data.active_hist:
-			active_mean += entry
-		active_mean = active_mean/len(data.active_hist)
+			means[4] += entry
+		means[4] = means[4]/len(data.active_hist)
 	
-	var food_peak = pos0*radar_scaling*food_mean
-	var water_peak = pos1*radar_scaling*water_mean
-	var sleep_peak = pos2*radar_scaling*sleep_mean
-	var stress_peak = pos3*radar_scaling*stress_mean
-	var activity_peak = pos4*radar_scaling*active_mean
+	WYDW_label.text = ""
+	WYDOK_label.text = ""
+	WYDP_label.text = ""
+	
+	for mean in range(0, 5):
+		if means[mean] >= 7:
+			WYDW_label.text += meanbeans[mean] + ", "
+		elif means[mean] >3:
+			WYDOK_label.text += meanbeans[mean] + ", "
+		else: 
+			WYDP_label.text += meanbeans[mean] + ", "
+	
+	var food_peak = pos0*radar_scaling*means[0]
+	var water_peak = pos1*radar_scaling*means[1]
+	var sleep_peak = pos2*radar_scaling*means[2]
+	var stress_peak = pos3*radar_scaling*means[3]
+	var activity_peak = pos4*radar_scaling*means[4]
 	
 	food_quad.polygon[0] = food_peak
 	food_quad.polygon[1] = (food_peak + water_peak)/4
@@ -144,4 +160,6 @@ func _process(_delta):
 	activity_quad.polygon[0] = activity_peak
 	activity_quad.polygon[1] = (activity_peak + food_peak)/4
 	activity_quad.polygon[3] = (activity_peak + stress_peak)/4
-
+	
+	
+		
