e1351437b85cbd80e89c7bf2750d4a13b25e6e10
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2024-01-03 16:26:20 +1300

    main menu quit button functionality added

--- Main Menu.gd
+++ Main Menu.gd
@@ -15,3 +15,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_mm_exit_app_button_pressed():
+	get_tree().quit()
