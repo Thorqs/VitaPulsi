18b88a0bc24720cd56c4938bf4de60e15d6ed92c
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2024-01-04 15:00:21 +1300

    Instantiable history item

--- History.gd
+++ History.gd
@@ -1,11 +1,18 @@
 extends VBoxContainer
 
 var mainMenu
+var history_contents = []
+var hist_grid: GridContainer
+@export var base_history: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mainMenu = get_node("../Main Menu")
-
+	hist_grid = get_node("History Scroll/History Contents")
+	
+	var test_item = base_history.instantiate()
+	hist_grid.add_child(test_item)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
