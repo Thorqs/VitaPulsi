ef46689dee5755905b189f4ef20ac8d7c3421b0f
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2024-01-09 19:22:55 +1300

    Data is saved in dictionary

--- Resources/UserData.gd
+++ Resources/UserData.gd
@@ -4,30 +4,35 @@ class_name UserData
 var save_file_path = "user://save/"
 var save_file_name = "UserData.tres"
 
-@export var history_data = []
-@export var day_hist = []
+var history_data: Dictionary
 
 func load_data():
 	DirAccess.make_dir_absolute(save_file_path)
 	if ResourceLoader.exists(save_file_path + save_file_name):
 		var temp = ResourceLoader.load(save_file_path + save_file_name)
 		history_data = temp.history_data
-		day_hist = temp.day_hist
 
-func save_data(values: Array[int]):
-	day_hist.append(Time.get_date_string_from_system())
-	history_data.append(values)
+func save_data(values: Array[int], day = Time.get_date_string_from_system()):
+	history_data[day] =  values
 	ResourceSaver.save(self, save_file_path + save_file_name)
 
 func clear_history():
 	history_data.clear()
-	day_hist.clear()
 
 func calc_means(days = 90):
+	# Generally speaking:
+	# - convert current date into unix time
+	# - step through dictionary in steps of 1 day of unix time (whatever that is)
+	# - Convert that day to string
+	# - try to access that day
+	# - if successful:
+	#	- increment num values
+	#	- add to ongoing means
+	#- else: nothing
 	var means = [0, 0, 0, 0, 0]
 	
-	if len(history_data) > 0:
-		if len(history_data) >= days:
+	if history_data.size() > 0:
+		if history_data.size() >= days:
 			for index in range(0,5):
 				for day in range(0, days):
 						means[index] += history_data[days][index]
@@ -35,7 +40,7 @@ func calc_means(days = 90):
 		else:
 			for index in range(0,5):
 				for entry in history_data:
-					means[index] += entry[index]
+					means[index] += history_data[entry][index]
 				means[index] = means[index]/len(history_data)
 	return means
 
